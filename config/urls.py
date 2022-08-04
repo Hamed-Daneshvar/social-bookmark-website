1209eff2f43505d3d0edccbf7d6ba51fc19119c9
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-08-04 21:56:15 +0430

    Feature: add register and profile for users

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('django.contrib.auth.urls')),
     path('account/', include('account.urls')),
 ]
+
+
+# load media file when DEBUG is True
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, 
+                          document_root=settings.MEDIA_ROOT)
