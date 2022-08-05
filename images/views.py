74fd42f98d9085f27582636c58a86dbaeaecc030
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-08-05 19:42:09 +0430

    feature: restrict like view by ajax decorator

--- images/views.py
+++ images/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from utils.decorators import ajax_required
 from .models import Image
 from .forms import ImageCreateForm
 
@@ -39,6 +40,7 @@ def image_detail(request, id, slug):
                   {'section': 'images',
                   'image':image,})
 
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
